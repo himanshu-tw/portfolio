e08cce8e6c0b6d2459bbd651183185017a5d2287
Himanshu <ht5620085@gmail.com>
2024-10-01 22:06:36 +0530

    changing project description of some projects

--- script.js
+++ script.js
@@ -16,11 +16,13 @@ window.addEventListener("scroll", () => {
     }
 });
 
+// scroll to top button functionality
 scrollToTop.addEventListener("click", () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
     window.history.replaceState({}, "", window.location.pathname);
 });
 
+// footer section date manipulation to show exact year
 date.innerText = new Date().getFullYear();
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -33,11 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const target = document.getElementById(targetId);
             target.scrollIntoView({ behavior: "smooth" });
 
-            history.pushState(
-                {},
-                "",
-                `${window.location.pathname}?section=${targetId}`
-            );
+            history.pushState({},"",`${window.location.pathname}?section=${targetId}`);
         });
     });
 });
